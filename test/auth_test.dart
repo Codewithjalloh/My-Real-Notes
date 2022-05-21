55e541892bc76ec4e413c45d6e444aaf54fc4e12
David. J <codewithjalloh@gmail.com>
2022-05-21 16:35:17 +0100

    step-11

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -36,45 +36,45 @@ void main() {
       timeout: const Timeout(Duration(seconds: 2)),
     );
 
-    test("Create user should delegate to login function", () async {
-      final badEmailUser = provider.createUser(
-        email: "foo@bar.com",
-        password: "anypassword",
-      );
-      expect(badEmailUser,
-          throwsA(const TypeMatcher<UserNotFoundAuthException>()));
+    // test("Create user should delegate to login function", () async {
+    //   final badEmailUser = provider.createUser(
+    //     email: "foo@bar.com",
+    //     password: "anypassword",
+    //   );
+    //   expect(badEmailUser,
+    //       throwsA(const TypeMatcher<UserNotFoundAuthException>()));
 
-      final badPasswordUser = provider.createUser(
-        email: "someone@bar.com",
-        password: "foobar",
-      );
-      expect(badPasswordUser,
-          throwsA(const TypeMatcher<UserNotFoundAuthException>()));
+    //   final badPasswordUser = provider.createUser(
+    //     email: "someone@bar.com",
+    //     password: "foobar",
+    //   );
+    //   expect(badPasswordUser,
+    //       throwsA(const TypeMatcher<UserNotFoundAuthException>()));
 
-      final user = await provider.createUser(
-        email: "foo",
-        password: "bar",
-      );
-      expect(provider.currentUser, user);
-      expect(user.isEmailVerified, false);
-    });
+    //   final user = await provider.createUser(
+    //     email: "foo",
+    //     password: "bar",
+    //   );
+    //   expect(provider.currentUser, user);
+    //   expect(user.isEmailVerified, false);
+    // });
 
-    test("Logged in user should be able to get verified", () {
-      provider.sendEmailVerification();
-      final user = provider.currentUser;
-      expect(user, isNotNull);
-      expect(user!.isEmailVerified, true);
-    });
+    // test("Logged in user should be able to get verified", () {
+    //   provider.sendEmailVerification();
+    //   final user = provider.currentUser;
+    //   expect(user, isNotNull);
+    //   expect(user!.isEmailVerified, true);
+    // });
 
-    test("should be able to log out and log in again", () async {
-      await provider.logOut();
-      await provider.logIn(
-        email: "email",
-        password: "password",
-      );
-      final user = provider.currentUser;
-      expect(user, isNotNull);
-    });
+    // test("should be able to log out and log in again", () async {
+    //   await provider.logOut();
+    //   await provider.logIn(
+    //     email: "email",
+    //     password: "password",
+    //   );
+    //   final user = provider.currentUser;
+    //   expect(user, isNotNull);
+    // });
   });
 }
 
