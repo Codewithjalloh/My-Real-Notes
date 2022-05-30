0eee6cdcb6a285201787049b00ff1bf890e011fa
David. J <codewithjalloh@gmail.com>
2022-05-30 21:22:12 +0100

    step-18

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -7,6 +7,7 @@ import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+// Updated Noteservice
 class NotesService {
   // Database? _db;
 
